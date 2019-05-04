dab9edec0d9d3f67d26c382cb14f05e785ff5d6e
Ali Falah <ali.fallah1992@yahoo.com>
2019-05-04 16:26:25 +0430

    'root'

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter , Route , Link } from 'react-router-dom';
+
+//COMPONENTS
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to="/">Home</Link><br/>
+                    <Link to="/posts">Posts</Link><br/>
+                    <Link to={{
+                        pathname:'/profile'
+                    }}>Profile</Link><br/>
+                    <hr/>
+                </header>
+                <Route path="/" exact component={Home}/>
+                <Route path="/posts" component={Posts}/>
+                <Route path="/profile" component={Profile}/>
+            </div>
+            
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+)
